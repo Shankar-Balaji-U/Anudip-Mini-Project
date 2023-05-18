27490680e26d05a0b30484474c854b4cd1497860
Shankar Balaji U <shankar369u@gmail.com>
2023-05-18 14:47:52 +0530

    setup a new project with defaults in application.properties

---
+++ socialize/src/main/java/com/anudipgroupproject/socialize/SocializeApplication.java
@@ -0,0 +1,13 @@
+package com.anudipgroupproject.socialize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocializeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocializeApplication.class, args);
+	}
+
+}
