5cc31224086b1bb8954da2920501d1e1975437f0
Shankar Balaji U <shankar369u@gmail.com>
2023-06-26 23:30:56 +0530

    Modified the form class and added new PostForm class

---
+++ socialize/src/main/java/com/anudipgroupproject/socialize/forms/PostForm.java
@@ -0,0 +1,71 @@
+package com.anudipgroupproject.socialize.forms;
+
+import static com.anudipgroupproject.socialize.utils.MediaManager.convertMultipartFileToFile;
+
+import java.io.IOException;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.anudipgroupproject.socialize.models.Post;
+
+import com.anudipgroupproject.socialize.models.User;
+import com.anudipgroupproject.socialize.validators.annotations.FileSize;
+
+import jakarta.validation.constraints.NotBlank;
+
+
+public class PostForm {
+	@NotBlank(message="User is required")
+    private User user;
+	
+	@NotBlank(message="Caption is required")
+	private String caption;
+	
+	@FileSize(max=2097152, message="Image size must be less than 2MB")
+	private MultipartFile image;
+	
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getCaption() {
+		return caption;
+	}
+
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+
+	public MultipartFile getImage() {
+		return this.image;
+	}
+	
+	public void setImage(MultipartFile image) {
+		this.image = image;
+	}
+	
+	public Post getEntity() throws IOException {
+		Post obj = new Post();
+	
+		if (this.getUser() != null) {
+			obj.setUser(this.getUser());
+		}
+		
+		if (this.getCaption() != null) {
+			obj.setCaption(this.getCaption());
+		}
+		
+		if (!this.getImage().isEmpty()) {
+			obj.setImage(convertMultipartFileToFile(this.getImage()));
+		}
+		return obj;
+	}
+
+	public String toString() {
+		return String.format("PostForm(caption=%s)", caption);
+	}
+}
--- socialize/src/main/java/com/anudipgroupproject/socialize/forms/UserForm.java
+++ socialize/src/main/java/com/anudipgroupproject/socialize/forms/UserForm.java
@@ -1,10 +1,18 @@
 package com.anudipgroupproject.socialize.forms;
 
+import static com.anudipgroupproject.socialize.utils.MediaManager.convertMultipartFileToFile;
+
+import java.io.IOException;
+
 import org.springframework.web.multipart.MultipartFile;
 
+//import org.springframework.web.multipart.MultipartFile;
+
 import com.anudipgroupproject.socialize.models.User;
+import com.anudipgroupproject.socialize.services.UserService;
 import com.anudipgroupproject.socialize.validators.annotations.FileSize;
-import com.anudipgroupproject.socialize.validators.annotations.UniqueUsername;
+//import com.anudipgroupproject.socialize.validators.annotations.FileSize;
+import com.anudipgroupproject.socialize.validators.annotations.Unique;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -13,7 +21,7 @@ import jakarta.validation.constraints.Size;
 
 public class UserForm {
     @NotBlank(message="Username is required")
-    @UniqueUsername(message="Username should be unique.")
+    @Unique(service=UserService.class, fieldName="username", message="Username should be unique.")
     private String username;
 
     private String displayname;
@@ -23,10 +31,11 @@ public class UserForm {
     private String password;
 
     @Pattern(regexp="\\d{10}", message="Mobile number must be a 10-digit number")
-    private String mobile_no;
-
+    private String mobileNo;
+    
+    @NotBlank(message="Email is required")
     @Email(message="Invalid email format")
-    private String email_id;
+    private String emailId;
 
     @FileSize(max=2097152, message="Image size must be less than 2MB")
     private MultipartFile image;
@@ -57,19 +66,19 @@ public class UserForm {
 	}
 
 	public String getMobileNo() {
-		return mobile_no;
+		return mobileNo;
 	}
 
-	public void setMobileNo(String mobile_no) {
-		this.mobile_no = mobile_no;
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
 	}
 
 	public String getEmailId() {
-		return email_id;
+		return emailId;
 	}
 
-	public void setEmailId(String email_id) {
-		this.email_id = email_id;
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
 	}
 
 	public MultipartFile getImage() {
@@ -80,7 +89,7 @@ public class UserForm {
 		this.image = image;
 	}
 	
-	public User getEntity() {
+	public User getEntity() throws IOException {
 		User obj = new User();
 		
 		if (this.getUsername() != null) {
@@ -95,8 +104,8 @@ public class UserForm {
 			obj.setDisplayname(this.getDisplayname());
 		}
 		
-		if (this.getImage() != null) {
-			obj.setTempImageFile(this.getImage());
+		if (!this.getImage().isEmpty()) {
+			obj.setImage(convertMultipartFileToFile(this.getImage()));
 		}
 		
 		if (this.getEmailId() != null) {
