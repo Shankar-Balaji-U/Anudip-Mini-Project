ab631ea3e876a8ade1883d1502fb191a1da5631e
Shankar Balaji U <shankar369u@gmail.com>
2023-07-27 12:06:23 +0530

    Updated the methods

--- socialize/src/main/java/com/anudipgroupproject/socialize/repositories/UserRepository.java
+++ socialize/src/main/java/com/anudipgroupproject/socialize/repositories/UserRepository.java
@@ -14,5 +14,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByUsername(String username);
 
 	boolean existsByUsername(String username);
+	boolean existsByUsernameAndIdNot(String username, Long id);
 }
 
